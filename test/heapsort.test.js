399b64eefa760b8d68facc7416ae58e3f8bb548d
ChelseaC13 <chelhelg@outlook.com>
2020-09-27 16:07:58 -0700

    done

--- test/heapsort.test.js
+++ test/heapsort.test.js
@@ -1,36 +1,38 @@
 const expect = require('chai').expect;
+const { heapsort } = require('../lib/heapsort');
 
-describe.skip("heapsort", function() {
-  // it("sorts an empty array", function() {
-  //   // Arrange 
-  //   const list = [];
 
-  //   // Act
-  //   const result = heapsort(list);
+describe("heapsort", function() {
+  it("sorts an empty array", function() {
+    // Arrange 
+    const list = [];
 
-  //   // Assert
-  //   expect(result).to.eql([]); // Note: use eql for deep equality
-  // });
+    // Act
+    const result = heapsort(list);
 
-  // it("can sort a 1-element array", function() {
-  //   // Arrange 
-  //   const list = [5];
+    // Assert
+    expect(result).to.eql([]); // Note: use eql for deep equality
+  });
 
-  //   // Act
-  //   const result = heapsort(list);
+  it("can sort a 1-element array", function() {
+    // Arrange 
+    const list = [5];
 
-  //   // Assert
-  //   expect(result).to.eql([5]);
-  // });
+    // Act
+    const result = heapsort(list);
+
+    // Assert
+    expect(result).to.eql([5]);
+  });
   
-  // it("can sort a 5-element array", function() {
-  //   // Arrange 
-  //   const list = [5, 27, 3, 16, -50];
+  it("can sort a 5-element array", function() {
+    // Arrange 
+    const list = [5, 27, 3, 16, -50];
 
-  //   // Act
-  //   const result = heapsort(list);
+    // Act
+    const result = heapsort(list);
 
-  //   // Assert
-  //   expect(result).to.eql([-50, 3, 5, 16, 27]);
-  // });
+    // Assert
+    expect(result).to.eql([-50, 3, 5, 16, 27]);
+  });
 });
